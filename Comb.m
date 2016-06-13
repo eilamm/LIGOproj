99fe2b876250047775d269b6a753d5a49fd6e06c
eilamm <eilam@umich.edu>
2016-06-13 17:18:47 -0400

    Added 3 methods to the Comb class: The constructor, the verify function, and the DIR (Date in range) function

--- Comb.m
+++ Comb.m
@@ -17,6 +17,51 @@ classdef Comb
     end
     
     methods
+        % constructor
+        function obj = Comb(c)
+            if (nargin > 0)
+                if isa(c, Comb)
+                    obj.init_date = c.init_date;
+                    obj.end_date = c.end_date;
+                    obj.low_b = c.low_b;
+                    obj.up_b = c.up_b;
+                    obj.offset = c.offset;
+                    obj.harm = c.harm;
+                    obj.ID = c.ID;
+                else
+                    obj.init_date = c(1);
+                    obj.end_date = c(2);
+                    obj.low_b = c(3);
+                    obj.up_b = c(4);
+                    obj.offset = c(5);
+                    obj.harm = c(6);
+                end
+            else
+                Disp(['Object properties not initialized']);
+            end
+        end
+        % verify: verifies that the properties of the object make sense;
+        % that the init_date is the same or before the end_date, that the
+        % lower bound is less than the upper_bound.
+        function r = verify(obj)
+            r = 1;
+            if (obj.end_date < obj.init_date)
+                disp(['Verification of Comb ', obj.ID, ' failed. The ' ...
+                    'end date, ', obj.end_date.date2str(), ' is ' ...
+                    'earlier than the initial date, ', ...
+                    obj.init_date.date2str()]);
+                r = 0;
+            elseif (obj.up_b <= obj.low_b)
+                disp(['Verification of Comb ', obj.ID, ' failed. The ' ...
+                    'lower bound, ', num2str(obj.low_b), ' is ' ...
+                    'not less than the upper bound, ', num2str(obj.up_b)]);
+                r = 0;
+            end
+            disp(['Comb ', num2str(obj.ID), ' is good.']);
+        end
+        % init_bins: A copy of the old "calc_bins", does the exact same
+        % thing, although the input arguments have been replaced by object
+        % properties (and slightly renamed).
         function bins = init_bins(obj) 
             % Calculate the number of frequencies, for preallocation.
             too_low = 0;
@@ -45,6 +90,16 @@ classdef Comb
                 end
             end
         end
-        
+        % DIR: Stands for Date In Range. Returns a 1 if "date" is between
+        % the init and end date of the object.
+        function r = DIR(obj, date)
+            if (date < obj.init_date)
+                r = 0;
+            elseif (date > obj.end_date)
+                r = 0;
+            else
+                r = 1;
+            end
+        end
     end
 end
