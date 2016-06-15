ae096c22d7b829bdba554d0f8bfbf1be5a56bfe3
eilamm <eilam@umich.edu>
2016-06-15 14:17:36 -0400

    Cleaned up LIGO_drive.m; it now calls combs(i).plot_xlabel/plot_ylabel/plot_title/plot_filename to retrieve the strings for those respective labels instead of hard_coding it. Also deleted some old commented-out code

--- LIGO_drive.m
+++ LIGO_drive.m
@@ -10,13 +10,6 @@
 clear variables; 
 close all;
 
-% SET THESE VALUES
-% offset = input('Enter offset from 0 (Hz): ');
-% HARMONIC = input('Enter harmonic (Hz): ');
-% START_FREQ = input('Enter lower bound frequency (Hz): ');
-% END_FREQ = input('Enter upper bound frequency (Hz): ');
-
-
 % SET START and END DATES for evaluation
 % Enter as follows: START_DATE = Date([dd mm yyyy]); same for END_DATE
 START_DATE = Date([1, 1, 2016]);
@@ -42,27 +35,6 @@ for x = 1:1:size(combs)
     combs(x) = combs(x).init();
 end
 
-% c1.init_date = START_DATE;
-% c1.end_date = END_DATE;
-% c2.init_date = START_DATE;
-% c2.end_date = END_DATE;
-% c3.init_date = START_DATE;
-% c3.end_date = END_DATE;
-% 
-% c1 = c1.init();
-% c2 = c2.init();
-% c3 = c3.init();
-% combs = [c1; c2; c3];
-% These HAVE to be multiples of 100. END_FREQ MUST be >= START_FREQ, and
-% both must be <= 4000. They determine which frequency range is looked
-% at, and thus which files are opened.
-% NOTE: CURRENTLY selecting any START_FREQ that isn't zero will give
-% inaccurate results. The functionality for indexing using the start
-% variable must be changed to rely upon START_FREQ for different start
-% values to work.
-% START_FREQ = 0;
-% END_FREQ = 4000;
-
 % Preallocate memory for the date array
 % A difference in months means a full month. The rest of rough_size is made
 % up by the days we go into the last month.
@@ -72,11 +44,7 @@ minutes = num_days/30*5;
 disp(['Working. This will take around ', num2str(minutes), ' minutes.']);
 disp(['Beginning time is ', datestr(now)]);
 
-
 combs = LIGO_body(combs);
-
-% rough_size
-%rough_size = 30;
 
 % plot(1:1:rough_size, day_averages, '*');
 for i = 1:1:size(combs)
@@ -102,19 +70,11 @@ for i = 1:1:size(combs)
 
     %Technically it's off by one, since day 1 is the start_date. Requires
     %slight tweaking.
-    xlabel(['Days since ', num2str(START_DATE.day), '/', num2str(START_DATE.month), '/', num2str(START_DATE.year)]);
-    ylabel('Average normalized power');
-    title(['Average normalized power for ', num2str(HARMONIC), ' Hz Harmonics with offset ', num2str(offset), ' Hz in range ', ...
-        num2str(START_FREQ), '-', num2str(END_FREQ), ' Hz between ', num2str(START_DATE.month), '/', ...
-        num2str(START_DATE.day), '/' , num2str(START_DATE.year), ' and ', num2str(END_DATE.month), '/', ...
-        num2str(END_DATE.day), '/' , num2str(END_DATE.year)]);
-
-    saveas(gcf, ['/home/eilam.morag/public_html/Avg_norm_pwr_for_', num2str(HARMONIC)...
-        , 'Hz_offset_', num2str(offset), 'Hz_range_', num2str(START_FREQ), '_to_', num2str(END_FREQ), 'Hz_dates_', ...
-        num2str(START_DATE.month), '-', num2str(START_DATE.day), '-' , num2str(START_DATE.year), ...
-        '_to_', num2str(END_DATE.month), '-', num2str(END_DATE.day), '-' , num2str(END_DATE.year), ...
-        '.png']);
-
+    xlabel(combs(i).plot_xlabel());
+    ylabel(combs(i).plot_ylabel());
+    title(combs(i).plot_title());
+    saveas(gcf, combs(i).plot_filename());
+    
     temp = START_DATE;
     
     disp(' ');
