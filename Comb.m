7aa6b53763ccb69742c1b2daad47fdbc75b24757
eilamm <eilam@umich.edu>
2016-06-17 14:44:39 -0400

    Comb.m now prints to terminal 'Saving file <filename>' when saveall() is called. Changed initial date on LIGO_drive.m

--- Comb.m
+++ Comb.m
@@ -171,7 +171,8 @@ classdef Comb
                 disp(['Creating directory ', path]);
                 mkdir(path);
             end
-            filename = [path, '/',o.plot_filename()];
+            filename = [path, '/', o.plot_filename()];
+            disp(['Saving file ', filename]);
             saveas(gcf, filename);
             
                 
