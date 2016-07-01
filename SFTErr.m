ea6a054ef1c4f94960a8ef0c646b9abbad17a935
eilamm <eilam@umich.edu>
2016-07-01 13:22:08 -0400

    Changed SFTErr.m so that if there was no data or there were no SFTs performed for a day, it would return NaN instead of Inf

--- SFTErr.m
+++ SFTErr.m
@@ -1,7 +1,8 @@
 % Author: Eilam Morag
 % June 13, 2016
 % Purpose: calculate another set of error bars, produced by calculating 
-% 1/sqrt(#SFTs_that_day * #teeth_in_comb)
+% 1/sqrt(#SFTs_that_day * #teeth_in_comb). If there was no data or no
+% SFTs, return NaN.
 
 function e = SFTErr(path, teeth)
     temp = '/sfts/tmp/*.sft';
@@ -10,7 +11,8 @@ function e = SFTErr(path, teeth)
     numSFTs = numel(listOfSFTs);
     
     e = 1/sqrt(numSFTs*teeth);
-    if (e == 0)
+    % If there was no data for a day or no SFTs, return NaN
+    if (isnan(teeth) || numSFTs == 0)
         e = NaN;
     end
 end
