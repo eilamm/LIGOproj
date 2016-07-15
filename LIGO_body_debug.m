31973e6e21194ca82fc4c474320dd3b3b9facfea
eilamm <eilam@umich.edu>
2016-07-15 14:26:40 -0400

    Just deleted some old commented out code

--- LIGO_body_debug.m
+++ LIGO_body_debug.m
@@ -7,29 +7,6 @@
 % Calculate all the frequencies we want to look at. 
 % Argument c is the combs array
 function c = LIGO_body_debug(c)
-    % Calculate all the bins to be looked at
-%     bins = calc_bins(HARMONIC, offset, START_FREQ, END_FREQ);
-%     num_freq = length(bins);
-    % start is a counter used to know which value of bins we are on.
-    % Increases with each iteration of the inner loop. Important to reset
-    % to 1 after a day is complete (each iteration of the outer loop)
-%     start = 1;
-    
-    % Number of days we are inspecting. Used to preallocate day_averages
-    
-    % Will contain the averaged normalized power for each day.
-%     day_averages = zeros(num_days, 1);
-%     day_errors = zeros(num_days, 1);
-    
-    % Initialize the total value (to be averaged later) to zero.
-%     total = 0;
-    % Initialize the square value (for std. dev) to zero
-%     square = 0;
-    
-    
-    
-%     fileA = START_FREQ - mod(START_FREQ, 100);
-%     fileZ = END_FREQ - 100;
     fileA = 0;
     fileZ = 4000 - 100;
     % Enter outer loop. Goes through each file for a day.
@@ -53,13 +30,6 @@ function c = LIGO_body_debug(c)
                     % the specific 100 Hz frequency range.
                     while ( (start <= length(c(j).bins)) && ...
                             (c(j).bins(start, 1) < freq + 100) )
-                        % Use the next value of the bins (second column, which is
-                        % the line number in the file) to index into the data
-                        % matrix. Take the value from the second column, which is
-                        % the normal power.
-        %                 data(bins(start, 2), :)
-        
-        
                         
                         % DEBUG PART BELOW
                         % Test to make sure that the frequency in the bins
@@ -94,9 +64,7 @@ function c = LIGO_body_debug(c)
                             
                         end
                         % DEBUG PART ABOVE
-                        
 
-                        
                         % DEBUG PART BELOW
                         % Test if the frequencies are constantly
                         % increasing, which they should be.
@@ -107,18 +75,12 @@ function c = LIGO_body_debug(c)
                         else
                             c(j).DEBUG_lastf = c(j).bins(start, 1);
                         end
-                        
                         % DEBUG PART ABOVE
-                
                         value = data(c(j).bins(start, 2), 2);
                         total = total + value;
                         square = square + value^2;
-                        start = start + 1;
-                        
-                        
+                        start = start + 1; 
                     end
-                    
-                    
                     c(j).total = total;
                     c(j).square = square;
                     c(j).start = start;
@@ -154,6 +116,4 @@ function c = LIGO_body_debug(c)
         end
         date = date.next_day();
     end
-%     disp('Errors');
-%     error_arr
 end
