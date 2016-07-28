c650a5a9c24b23b2742013ff7735443d19613195
eilamm <eilam@umich.edu>
2016-07-28 16:38:48 -0400

    Fixed bug in disp

--- newChannel.m
+++ newChannel.m
@@ -9,7 +9,7 @@ function newChannel(channel)
     uid = getenv('USER');
     path = ['/home/', uid, '/public_html/CombInv/Channels/', channel, '/'];
     if (exist(path, 'dir') == 0)
-        disp('New channel: ', channel);
+        disp(['New channel: ', channel]);
         disp(['Creating directory', path]);
         mkdir(path);
         genCombNavHTML(path, channel);
