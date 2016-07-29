bd671180c340a996a5c70b2755ba8f59badd6226
eilamm <eilam@umich.edu>
2016-07-29 10:47:30 -0400

    Changed dates in order to test diffDates()

--- LIGO_drive.m
+++ LIGO_drive.m
@@ -13,7 +13,7 @@ firstTimeSetup();
 % SET START and END DATES for evaluation
 % Enter as follows: START_DATE = Date([dd mm yyyy]); same for END_DATE
 START_DATE = Date([26, 2, 2015]);
-END_DATE = Date([5, 7, 2016]);
+END_DATE = Date([14, 5, 2016]);
 % You may hard-code the CHANNEL name here instead of entering it at the
 % beginning of the program's execution. Capitalization does not matter,
 % lower-case will be converted to upper-case.
